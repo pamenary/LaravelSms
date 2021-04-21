ee00b528e5f563e775614c03d3600e0b4f725339
Mahdi Pishguy <pishguy@gmail.com>
2021-04-21 10:56:05 +0430

    Update TsmsGateway.php

--- src/Gateways/TsmsGateway.php
+++ src/Gateways/TsmsGateway.php
@@ -8,31 +8,33 @@ namespace Pamenary\LaravelSms\Gateways;
  * Date: 12/23/2016
  * Time: 12:51 PM
  */
-class TsmsGateway extends GatewayAbstract {
+class TsmsGateway extends GatewayAbstract
+{
 
-	/**
-	 * AfeGateway constructor.
-	 */
-	public function __construct() {
+    /**
+     * AfeGateway constructor.
+     */
+    public function __construct()
+    {
 
-		$this->webService  = config('sms.gateway.tsms.webService');
-		$this->username    = config('sms.gateway.tsms.username');
-		$this->password    = config('sms.gateway.tsms.password');
-		$this->from        = config('sms.gateway.tsms.from');
-	}
+        $this->webService = config('sms.gateway.tsms.webService');
+        $this->username = config('sms.gateway.tsms.username');
+        $this->password = config('sms.gateway.tsms.password');
+        $this->from = config('sms.gateway.tsms.from');
+    }
 
 
-	/**
-	 * @param array $numbers
-	 * @param       $text
-	 * @param bool  $isflash
-	 *
-	 * @return mixed
-	 * @internal param $to | array
-	 */
-	public function sendSMS( array $numbers, $text, $isflash = false ) {
-        //dd($numbers, $text);
-
+    /**
+     * @param array $numbers
+     * @param       $text
+     * @param bool $isflash
+     *
+     * @return mixed
+     * @internal param $to | array
+     */
+    public function sendSMS(array $numbers, $text, $isflash = false)
+    {
+        if (!$this->GetCredit()) return;
         try {
             $api = new \SoapClient($this->webService);
             $api->sendSms(
@@ -44,24 +46,25 @@ class TsmsGateway extends GatewayAbstract {
                 [],
                 rand()
             );
-        } catch( SoapFault $ex ) {
+        } catch (SoapFault $ex) {
             echo $ex->faultstring;
         }
-	}
+    }
 
 
-	/**
-	 * @return mixed
-	 */
-	public function getCredit() {
-		try {
-			$client = new \SoapClient( $this->webService );
+    /**
+     * @return mixed
+     */
+    public function getCredit()
+    {
+        try {
+            $client = new \SoapClient($this->webService);
 
-			$result = $client->GetRemainingCredit( array('Username' => $this->username, 'Password' => $this->password) );
-			return $result->GetRemainingCreditResult;
-		} catch( SoapFault $ex ) {
-			echo $ex->faultstring;
-		}
-	}
+            $result = $client->UserInfo($this->username,$this->password);
+            return $result[0]->credit;
+        } catch (SoapFault $ex) {
+            echo $ex->faultstring;
+        }
+    }
 
 }
